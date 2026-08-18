ac94c05f86ddd9a5b7f38e1ffb1331e98f2b2339
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2026-08-18 23:11:30 +0300

    fix db

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,9 +1,10 @@
-import {neon} from "@neondatabase/serverless";
-import {drizzle} from "drizzle-orm/neon-http";
-import {env} from "@/env";
-import * as schema from "@/db/schema";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
-const sql = neon(env.DATABASE_URL);
+import * as schema from "./schema";
 
-export const db = drizzle(sql, {schema});
-export type Database = typeof db;
+export const db = drizzle({
+  connection: process.env.DATABASE_URL!,
+  ws,
+  schema,
+});
